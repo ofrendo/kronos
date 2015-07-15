537f5458f8805b7b2bf7a3bd62ca770409a0bde6
Oliver Frendo <oliver.frendo@sap.com>
2015-07-15 09:09:25 -0400

    Removed activemq data and modified gitignore

--- src/kronos/test/TestSuiteDataSources.java
+++ src/kronos/test/TestSuiteDataSources.java
@@ -1,11 +1,58 @@
 package kronos.test;
 
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
 public class TestSuiteDataSources {
 
 	public static void main(String[] args) {
+		try {
+			System.out.println("Trying to connect...");
 		
-		System.out.println("Hello world!");
+			String conStr = "tcp://localhost:61616";
+			ConnectionFactory connectionFactory =
+					new ActiveMQConnectionFactory (conStr);
 		
+			// Create a Connection
+			Connection connection = connectionFactory.createConnection();
+			connection.start();
+		
+			//connection.setExceptionListener(this);
+        
+			// Create a Session
+			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+			// Create the destination (Topic or Queue)
+			Destination destination = session.createQueue("m_orders2");
+			
+			// Create a MessageConsumer from the Session to the Topic or Queue
+			MessageConsumer consumer = session.createConsumer(destination);
+
+	        // Wait for a message
+	        Message message = consumer.receive(50000);
+	
+	        if (message instanceof TextMessage) {
+	            TextMessage textMessage = (TextMessage) message;
+	            String text = textMessage.getText();
+	            System.out.println("Received text: " + text);
+	        } else {
+	            System.out.println("Received: " + message);
+	        }
+	
+	        consumer.close();
+	        session.close();
+	        connection.close();
+	    } catch (Exception e) {
+	        System.out.println("Caught: " + e);
+	        e.printStackTrace();
+	    }
 	}
 
 }
