e74562224e7d9deb7e8085569d795dce24434845
Jong23 <joernhuebner@freenet.de>
2015-07-16 19:42:50 +0200

    erp data

--- src/kronos/sim/MessageHandler.java
+++ src/kronos/sim/MessageHandler.java
@@ -1,27 +1,73 @@
 package kronos.sim;
 
+import java.io.StringReader;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 
-public class MessageHandler extends Observable implements Observer {
+import kronos.model.ERPData;
+import kronos.util.Log;
+
+
+public class MessageHandler implements Observer {
 	
 	protected MessageHandler() {}
 	
-	protected synchronized void onMessage(String text) {
+	protected synchronized void onMessage(String text, SimSourceType type) {
 		//Use something like this in observers
 		//SimSource source = (SimSource) o;
 		//switch (source.getType()) {
 		//case erpData:
 		//}
-		this.notifyObservers(text);
+
+		
+		switch (type) {
+		case erpData:
+			handleERP(text);
+			break;
+		case machineData:
+			
+			break;
+			
+		case saData:
+
+			break;
+		default:
+			break;
+		}
+		
+		
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
 		String text = (String) arg;
-		onMessage(text);
+		onMessage(text, ((SimSource) o).getType());
 	}
-
 	
+	public void handleERP(String xml){
+		try {	
+			JAXBContext _ctx = JAXBContext.newInstance(ERPData.class);
+			Unmarshaller _unmarshaller = _ctx.createUnmarshaller();
+			
+			StringReader sReader = new StringReader(xml); 
+			ERPData erpData2 = (ERPData) _unmarshaller.unmarshal(sReader);
+			
+			Log.info("###################################");
+			Log.info("Material: " + erpData2.getMaterialNumber());
+			Log.info("Customer: "+ erpData2.getCustomerNumber());
+			Log.info("Order: " + erpData2.getOrderNumber());
+			Log.info("Timestamp: " + erpData2.getTimeStamp());
+			
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
 }
