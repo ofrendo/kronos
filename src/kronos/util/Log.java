19dfa6d52a9c0c3433bbcdc32c102ef6a8f00f71
Oliver Frendo <oliver.frendo@sap.com>
2015-07-18 15:18:29 -0400

    removed annoying SLF4J Error message

--- src/kronos/util/Log.java
+++ src/kronos/util/Log.java
@@ -1,8 +1,11 @@
 package kronos.util;
 
+import java.io.PrintStream;
+
 import org.apache.log4j.Logger;
 
 public class Log {
+	protected static boolean turnedOffSLF4J = turnOffSLF4J();
 	
 	/* Get actual class name to be printed on */
 	private static Logger log = Logger.getLogger(Log.class.getName());
@@ -19,5 +22,18 @@ public class Log {
     public static synchronized void fatal(String text) {
         log.fatal(text);
     }
-   
+    
+    private static boolean turnOffSLF4J() {
+    	PrintStream filterOut = new PrintStream(System.err) {
+    	    public void println(String l) {
+    	        if (! l.startsWith("SLF4J") )
+    	            super.println(l);
+    	    }
+    	};
+    	System.setErr(filterOut);
+    	return true;
+    }
+    
+    
+    
 }
